2d77f1f4e419e9edead72a3420eeeea2f620d564
Glauber Perez <glauber2070@hotmail.com>
2024-08-18 14:00:20 -0300

    idk what is happening

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+// axioss
+const axios = require('axios');
+// cheerio
+const cheerio = require('cheerio');
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/t/:req', function(req, res, next) {
+  getTranslation('en','pt',req.params.req);
   res.send({req : req.params.req});
 });
 
-
+function getTranslation(sl,tl,text) {
+  axios.get('https://translate.google.com/?sl='+sl+'&tl='+tl+'&text='+text+'&op=translate')
+  .then(function (response) {
+    const $ = cheerio.load(response.data);
+    const translation = $('span.ryNqvb');
+    console.log("t:"+translation);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 
 module.exports = router;
